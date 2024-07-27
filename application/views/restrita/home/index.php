13130a033f75c4eabad4394414c8ce9213802dd5
Tavilo Breno <breno_wk2@hotmail.com>
2024-07-27 00:01:08 -0300

    Realizar o fatiamento do template da área restrita

--- application/views/restrita/home/index.php
+++ application/views/restrita/home/index.php
@@ -1,1 +1,16 @@
-<h1>Você está na home dá área restrita</h1>
+<?php $this->load->view('restrita/layout/navbar'); ?>
+<?php $this->load->view('restrita/layout/sidebar'); ?>
+
+
+
+		<!-- Main Content -->
+		<div class="main-content">
+			<section class="section">
+				<div class="section-body">
+					<!-- add content here -->
+				</div>
+			</section>
+			<?php $this->load->view('restrita/layout/sidebar_settings'); ?>
+		</div>
+
+
